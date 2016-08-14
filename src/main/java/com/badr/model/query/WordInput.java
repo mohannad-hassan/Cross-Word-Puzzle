75b71ef7f5725a2869d2bb9eb96f03ad7917b2f5
Mohannad A. Hassan <mohannad.hassan@badrit.com>
2016-08-14 21:11:37 +0200

    Create WordInput

---
+++ src/main/java/com/badr/model/query/WordInput.java
@@ -0,0 +1,26 @@
+package com.badr.model.query;
+
+import java.util.List;
+
+/**
+ * Encapsulates a query about a word in a cross-word puzzle. 
+ * Created by mohannadhassan on 8/14/16.
+ */
+public class WordInput {
+
+    private int wordLength;
+    private List<CharacterInput> characterInputs;
+
+    public WordInput(int wordLength, List<CharacterInput> characterInputs) {
+        this.wordLength = wordLength;
+        this.characterInputs = characterInputs;
+    }
+
+    public int getWordLength() {
+        return wordLength;
+    }
+
+    public List<CharacterInput> getCharacterInputs() {
+        return characterInputs;
+    }
+}
