fa0e0b41827eebbddd46cd62049c488f364f88e2
Mohannad A. Hassan <mohannad.hassan@badrit.com>
2016-08-15 01:35:34 +0200

    Implement data reading in DataReader

--- src/main/java/com/badr/data_reader/DataReader.java
+++ src/main/java/com/badr/data_reader/DataReader.java
@@ -1,7 +1,31 @@
 package com.badr.data_reader;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Created by mohannadhassan on 8/15/16.
  */
 public class DataReader {
+
+    private Scanner scanner;
+
+    public DataReader(Readable dataSource) {
+        openScanner(dataSource);
+    }
+
+    private void openScanner(Readable dataSource) {
+        scanner = new Scanner(dataSource);
+        scanner.useDelimiter("\\s+");
+    }
+
+    public List<String> read() {
+        LinkedList<String> list = new LinkedList<>();
+        while (scanner.hasNext()) {
+            String word = scanner.next();
+            list.add(word);
+        }
+        return list;
+    }
 }
--- src/test/java/com/badr/data_reader/DataReaderTest.java
+++ src/test/java/com/badr/data_reader/DataReaderTest.java
@@ -2,11 +2,40 @@ package com.badr.data_reader;
 
 import junit.framework.TestCase;
 
-import static org.junit.Assert.*;
+import java.io.*;
+import java.util.List;
 
 /**
  * Created by mohannadhassan on 8/15/16.
  */
-public class DataReaderTest extends TestCase{
+public class DataReaderTest extends TestCase {
 
+    private BufferedReader br;
+    private DataReader dataReader;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        try {
+            ClassLoader classLoader = getClass().getClassLoader();
+            String filePath =  classLoader.getResource("com.badr.data_reader/small_data_file").getFile();
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
+        }
+        catch ( IOException e) {
+            System.err.println("Caught exception while opening file: " + e);
+        }
+        dataReader = new DataReader(br);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        br.close();
+    }
+
+    @org.junit.Test
+    public void testSmallDataFile() {
+        List<String> words = dataReader.read();
+        assertEquals(words.size(), 9);
+    }
 }
