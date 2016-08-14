4b89eaabe5882c5f9478e586a95d40428babbc5d
Mohannad A. Hassan <mohannad.hassan@badrit.com>
2016-08-14 02:03:22 +0200

    Handle adding words indexed by characters in AlphabetIndex

--- src/main/java/com/badr/model/AlphabetIndex.java
+++ src/main/java/com/badr/model/AlphabetIndex.java
@@ -1,11 +1,40 @@
 package com.badr.model;
 
+import java.util.HashMap;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * Created by mohannadhassan on 8/14/16.
  */
 public class AlphabetIndex {
 
+    private HashMap<Character, Set<String>> characterToWordsIndex;
+
+    public AlphabetIndex() {
+        characterToWordsIndex = new HashMap<Character, Set<String>>();
+    }
+
     public void put(Character c, String word) {
-        
+        if (c == null) {
+            throw new IllegalArgumentException("Input character 'c' may not be null");
+        }
+        if (word == null) {
+            throw new IllegalArgumentException("Input word may not be null");
+        }
+
+        Set<String> words = getWords(c);
+        words.add(word);
+    }
+
+    private Set<String> getWords(Character c) {
+        Set<String> words = characterToWordsIndex.get(c);
+
+        if (words == null) {
+            words = new TreeSet<String>();
+            characterToWordsIndex.put(c, words);
+        }
+
+        return words;
     }
 }
