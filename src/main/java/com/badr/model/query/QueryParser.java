75df4b14dd1d485e3dc71bc01069aa92fe3d0950
Mohannad A. Hassan <mohannad.hassan@badrit.com>
2016-08-14 22:04:06 +0200

    Handle parsing queries into WordInput in QueryParser

--- src/main/java/com/badr/model/query/QueryParser.java
+++ src/main/java/com/badr/model/query/QueryParser.java
@@ -1,7 +1,29 @@
 package com.badr.model.query;
 
+import java.util.ArrayList;
+
 /**
  * Created by mohannadhassan on 8/14/16.
  */
 public class QueryParser {
+
+    public WordInput parse(String input) throws MalformedQueryException {
+        if (input == null) {
+            throw new IllegalArgumentException("input may not be null");
+        }
+
+        ArrayList<CharacterInput> characterInputs = new ArrayList<>();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c != '-') {
+                if (Character.isLetter(c)) {
+                    characterInputs.add(new CharacterInput(i, c));
+                }
+                else {
+                    throw new MalformedQueryException(c, input);
+                }
+            }
+        }
+        return new WordInput(input.length(), characterInputs);
+    }
 }
--- src/test/java/com/badr/model/query/QueryParserTest.java
+++ src/test/java/com/badr/model/query/QueryParserTest.java
@@ -2,11 +2,62 @@ package com.badr.model.query;
 
 import junit.framework.TestCase;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by mohannadhassan on 8/14/16.
  */
 public class QueryParserTest extends TestCase {
 
+    private QueryParser parser;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        parser = new QueryParser();
+    }
+
+    @org.junit.Test
+    public void testSuccessfulCase() {
+        try {
+            WordInput input = parser.parse("--a--");
+
+            assertEquals(input.getWordLength(), 5);
+
+            List<CharacterInput> characterInputs = input.getCharacterInputs();
+
+            assertEquals(characterInputs.size(), 1);
+            assertEquals(characterInputs.get(0).getCharacter(), 'a');
+            assertEquals(characterInputs.get(0).getPosition(), 2);
+
+            input = parser.parse("a-a-");
+
+            assertEquals(input.getWordLength(), 4);
+
+            characterInputs = input.getCharacterInputs();
+
+            assertEquals(characterInputs.size(), 2);
+            assertEquals(characterInputs.get(0).getCharacter(), 'a');
+            assertEquals(characterInputs.get(0).getPosition(), 0);
+            assertEquals(characterInputs.get(1).getCharacter(), 'a');
+            assertEquals(characterInputs.get(1).getPosition(), 2);
+        }
+        catch (MalformedQueryException e) {
+            fail("Caught a " + MalformedQueryException.class + ": " + e);
+        }
+    }
+
+    @org.junit.Test()
+    public void testWhiteSpaceInput() {
+        try {
+            parser.parse("  --  ");
+        }
+        catch (MalformedQueryException e) { }
+        catch (Exception e) {
+            fail("Didn't throw the expected " + MalformedQueryException.class);
+        }
+    }
 }
