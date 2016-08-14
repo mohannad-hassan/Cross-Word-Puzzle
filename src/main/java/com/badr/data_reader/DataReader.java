a3658c44d4004c74b2c4070835dfcbc3c2cb06c9
Mohannad A. Hassan <mohannad.hassan@badrit.com>
2016-08-15 01:05:21 +0200

    Create DataReader to handle reading data files to Indexes

---
+++ src/main/java/com/badr/data_reader/DataReader.java
@@ -0,0 +1,7 @@
+package com.badr.data_reader;
+
+/**
+ * Created by mohannadhassan on 8/15/16.
+ */
+public class DataReader {
+}
---
+++ src/test/java/com/badr/data_reader/DataReaderTest.java
@@ -0,0 +1,12 @@
+package com.badr.data_reader;
+
+import junit.framework.TestCase;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mohannadhassan on 8/15/16.
+ */
+public class DataReaderTest extends TestCase{
+
+}
