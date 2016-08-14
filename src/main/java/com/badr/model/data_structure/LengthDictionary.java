7a5417a1217725c0ea158acce96854b9face2dd3
Mohannad A. Hassan <mohannad.hassan@badrit.com>
2016-08-14 04:40:10 +0200

    Create LengthDictionar

---
+++ src/main/java/com/badr/model/data_structure/LengthDictionary.java
@@ -0,0 +1,7 @@
+package com.badr.model.data_structure;
+
+/**
+ * Created by mohannadhassan on 8/14/16.
+ */
+public class LengthDictionary {
+}
---
+++ src/test/java/com/badr/model/data_structure/LengthDictionaryTest.java
@@ -0,0 +1,17 @@
+package com.badr.model.data_structure;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mohannadhassan on 8/14/16.
+ */
+public class LengthDictionaryTest extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
