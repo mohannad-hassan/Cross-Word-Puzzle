66ef3e22b221f91d045275195f63041f270ec724
Mohannad A. Hassan <mohannad.hassan@badrit.com>
2016-08-14 18:11:38 +0200

    Implement construction and words population in LengthDictionary

--- src/main/java/com/badr/model/data_structure/LengthDictionary.java
+++ src/main/java/com/badr/model/data_structure/LengthDictionary.java
@@ -1,7 +1,42 @@
 package com.badr.model.data_structure;
 
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Created by mohannadhassan on 8/14/16.
  */
 public class LengthDictionary {
+    private HashMap<Integer, MultiCharIndex> index;
+
+    public LengthDictionary(List<String> words) {
+        if (words == null) {
+            throw new IllegalArgumentException("words may not be null");
+        }
+        index = new HashMap<Integer, MultiCharIndex>();
+        populateWords(words);
+    }
+
+    private void populateWords(List<String> words) {
+        for (String word :
+                words) {
+            int length = word.length();
+            if (length == 0) {
+                throw new IllegalArgumentException("input words contain an empty string");
+            }
+
+            MultiCharIndex lengthIndex = getMultiCharIndex(word.length());
+            lengthIndex.add(word);
+        }
+    }
+
+    private MultiCharIndex getMultiCharIndex(int length) {
+        MultiCharIndex multiCharIndex = index.get(length);
+
+        if (multiCharIndex == null) {
+            multiCharIndex = new MultiCharIndex(length);
+            index.put(length, multiCharIndex);
+        }
+        return multiCharIndex;
+    }
 }
