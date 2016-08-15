d177e9348bf0a37b7151773a761e0d18cc5d0a12
Mohannad A. Hassan <mohannad.hassan@badrit.com>
2016-08-15 02:45:35 +0200

    Extract menu logic from Main to SuggestionDriver

--- src/main/java/com/badr/cross_word_suggest/SuggestionDriver.java
+++ src/main/java/com/badr/cross_word_suggest/SuggestionDriver.java
@@ -1,8 +1,136 @@
 package com.badr.cross_word_suggest;
 
+import com.badr.data_reader.DataReader;
+import com.badr.model.data_structure.LengthDictionary;
+import com.badr.model.query.MalformedQueryException;
+import com.badr.model.query.QueryParser;
+import com.badr.model.query.WordInput;
+
+import java.io.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by mohannadhassan on 8/15/16.
  */
 public class SuggestionDriver {
-    
+
+    private LengthDictionary dictionary;
+    private Reader dataFileReadable;
+    private BufferedReader userPromptReader;
+
+    private QueryParser queryParser;
+
+    public SuggestionDriver(Reader dataFileReadable, Reader userPromptReader) {
+        this.dataFileReadable = dataFileReadable;
+        this.userPromptReader = new BufferedReader(userPromptReader);
+
+        queryParser = new QueryParser();
+    }
+
+    public void start() {
+        readDataFile();
+        startMenu();
+    }
+
+    private void readDataFile() {
+        DataReader dataReader = new DataReader(dataFileReadable);
+        List<java.lang.String> words = dataReader.read();
+
+        try {
+            dataFileReadable.close();
+        }
+        catch (IOException e) {
+            System.err.println("Exception while closing stream: " + dataFileReadable);
+            e.printStackTrace();
+        }
+        dictionary = new LengthDictionary(words);
+    }
+
+    private void startMenu() {
+        printUsage();
+
+        QueryParser parser = new QueryParser();
+
+        while (true) {
+            String s = readLine();
+
+            String [] input = s.split("[ ]+");
+            String command = input[0].toLowerCase();
+
+            if (command.equals("q")) {
+                quit();
+                break;
+            }
+
+            if (input.length != 2) {
+                System.out.println("Expected an input");
+                printUsage();
+            }
+            else  {
+                processInput(input[1]);
+            }
+        }
+    }
+
+    private String readLine() {
+        String s = "";
+
+        while (s.isEmpty()) {
+            try {
+                s = userPromptReader.readLine();
+                if (s == null) {
+                    quit();
+                }
+                else  {
+                    s = s.trim();
+                }
+            } catch (IOException e) {
+                //Suppress exception
+                e.printStackTrace();
+            }
+        }
+
+        return s;
+    }
+
+    private void printUsage() {
+        System.out.println("s: Enter query.\n q: quit");
+    }
+
+    private void quit() {
+        System.out.println("...");
+        try {
+            userPromptReader.close();
+        } catch (IOException e) {
+            System.err.println("Got an exception while closing stream: " +  userPromptReader);
+            e.printStackTrace();
+        }
+        System.exit(0);
+    }
+
+    private void processInput(String word) {
+        WordInput wordInput;
+
+        try {
+            wordInput = queryParser.parse(word);
+        }
+        catch (MalformedQueryException e) {
+            System.out.println("Input \"" + word + "\" is not a correct input");
+            printUsage();
+            return;
+        }
+
+        Set<String> words = dictionary.search(wordInput);
+
+        System.out.println("Got " + words.size() + " words");
+        int length = words.size() > 100 ? 100 : words.size();
+        System.out.println("Printing first " + length + " words");
+
+        Iterator<String> iterator = words.iterator();
+        while (iterator.hasNext() && length -- > 0) {
+            System.out.println(iterator.next());
+        }
+    }
 }
--- src/main/java/com/badr/main/Main.java
+++ src/main/java/com/badr/main/Main.java
@@ -1,14 +1,8 @@
 package com.badr.main;
 
-import com.badr.model.data_structure.LengthDictionary;
-import com.badr.model.query.MalformedQueryException;
-import com.badr.model.query.QueryParser;
-import com.badr.model.query.WordInput;
+import com.badr.cross_word_suggest.SuggestionDriver;
 
 import java.io.*;
-import java.util.Iterator;
-import java.util.Scanner;
-import java.util.Set;
 
 /**
  * Created by mohannadhassan on 8/14/16.
@@ -23,85 +17,20 @@ public class Main {
 
         String inputFilePath = args[0];
 
-        Scanner scanner = null;
+        BufferedReader dataInputReader = null;
+
         try {
-            scanner = new Scanner(new BufferedReader(new FileReader(inputFilePath)));
-            scanner.useDelimiter("\\s+");
+            dataInputReader = new BufferedReader(new InputStreamReader(new FileInputStream(inputFilePath)));
         }
         catch (FileNotFoundException e) {
             System.err.println("Input file does not exist: " + inputFilePath);
+            System.exit(-1);
         }
 
-        long time = System.currentTimeMillis();
-        LengthDictionary dictionary = new LengthDictionary();
-
-        int count = 0;
-
-        while (scanner.hasNext()) {
-            dictionary.add(scanner.next());
-            count++;
-            if (count % 10000 == 0) {
-                System.out.println("Processed " + count + " words");
-            }
-        }
-        scanner.close();
-
-        System.out.println("s: Enter query.\n q: quit");
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        QueryParser parser = new QueryParser();
-
-        while (true) {
-            String s = null;
-            try {
-                s = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            String [] input = s.split("[ ]+");
-            String command = input[0].toLowerCase();
-            if (command.equals("q")) {
-                System.out.println("...");
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                break;
-            }
-            else if(command.equals("s")) {
-                if (input.length != 2) {
-                    //Error
-                }
-                else  {
-                    String word = input[1];
-
-                    WordInput wordInput = null;
-
-                    try {
-                        wordInput = parser.parse(word);
-                    }
-                    catch (MalformedQueryException e) {
-                        // Wrong Input
-                    }
-
-                    Set<String> words = dictionary.search(wordInput);
-
-                    System.out.println("Got " + words.size() + " words");
-                    int length = words.size() > 100 ? 100 : words.size();
-
-                    Iterator<String> iterator = words.iterator();
-                    while (iterator.hasNext() && length -- > 0) {
-                        System.out.println(iterator.next());
-                    }
-                }
-            }
-        }
+        BufferedReader userPromptStream = new BufferedReader(new InputStreamReader(System.in));
 
 
-        long time2 = System.currentTimeMillis();
-
-        System.out.println(count + " words at " + (time2 - time) / 1000.0 + " seconds.");
+        SuggestionDriver suggestionDriver = new SuggestionDriver(dataInputReader, userPromptStream);
+        suggestionDriver.start();
     }
 }
